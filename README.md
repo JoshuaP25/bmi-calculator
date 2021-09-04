6e920fd30cafbe9941f9c6862eaadaa00b5cb809
JPerez <imbajoshuaperez@gmail.com>
2021-09-04 18:33:22 +0800

    Add styling and README

---
+++ README.md
@@ -0,0 +1,18 @@
+# BMI Calculator
+
+## Table of contents
+
+- [General Info](#general-information)
+- [Technologies](#technologies)
+
+## General Info
+
+A responsive webpage for BMI calculator.
+
+## Technologies
+
+Project is created with
+
+- HTML5
+- CSS3
+- Javascript
--- script.js
+++ script.js
@@ -8,18 +8,31 @@ function displayResult() {
     const w = document.getElementById("weight").value;
     const h = document.getElementById("height").value;
     const result = calculateBMI(w, h).toFixed(1);
-    total.innerHTML = result;
+    
+    if(isNaN(result)) {
+        total.innerHTML = "Warning!";
+    }
+    else {
+        total.innerHTML = 'BMI = ' + result;
+    }
     if(result < 18.5) {
         display.innerHTML = "Underweight";
+        display.style.color = "#1abc9c";
     } 
     else if(result >= 18.5 && result <= 24.9) {
         display.innerHTML = "Healthy";
+        display.style.color = "#27ae60";
     }
     else if(result >= 25 && result <= 29.9) {
         display.innerHTML = "Overweight";
+        display.style.color = "#f39c12";
     }
-    else {
+    else if(result >= 30) {
         display.innerHTML = "Obese";
+        display.style.color = "#c0392b";
+    } else {
+        display.innerHTML = "Invalid input";
+        display.style.color = "#e74c3c";
     }
 }
 
